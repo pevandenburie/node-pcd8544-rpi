bfc83604d9804c483c85e3254e81049aec19dc59
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2016-02-22 19:50:57 +0000

    Fixed module export in index.js, and moved the test.

--- index.js
+++ index.js
@@ -2,8 +2,5 @@
 var bindings = require('bindings');
 var lcd = bindings('pcd8544_rpi');
 
-lcd.init();
-//lcd.clear();
-lcd.drawstring(0, 0, "Hello Raspi!");
-lcd.drawstring(0, 28, "Hello Tintim!");
-lcd.display();
+module.exports = lcd;
+
